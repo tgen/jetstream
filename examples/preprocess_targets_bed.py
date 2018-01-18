4b10adea46931030f705e6bbc7c16723fec98403
Ryan Richholt <rrichholt@tgen.org>
2018-01-17 18:44:26 -0700

    Preprocess script now writes to stdout

--- examples/preprocess_targets_bed.py
+++ examples/preprocess_targets_bed.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import os
 from jetstream.formats import intervals
 
 
@@ -20,20 +19,12 @@ def preprocess_bed(path, out_path):
         i['seqname'] = remove_prefix(i['seqname'], 'chr')
         i['seqname'] = i['seqname'].replace('_', '.')
 
-    with open(out_path, 'w') as fp:
-        print(intervals.to_bed(bed), file=fp)
-
-    return out_path
+    print(intervals.to_bed(bed))
 
 
 if __name__ == '__main__':
     bed_path = sys.argv[1]
-    out_path = sys.argv[2]
-
-    if os.path.exists(out_path):
-        raise OSError("{} already exists".format(out_path))
-    else:
-        preprocess_bed(bed_path, out_path)
+    preprocess_bed(bed_path)
 
 
 
