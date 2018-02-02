b2674371f7b63502b4cc7bb484fc9d814e4f8425
Ryan Richholt <rrichholt@tgen.org>
2018-02-02 12:44:19 -0700

    plugin_dir and plugin_id_pattern are top level package attributes now

--- jetstream/__init__.py
+++ jetstream/__init__.py
@@ -1,5 +1,11 @@
 """TODO: update main docstring
 """
+import pkg_resources
+
+# I'm adding some package wide variables here in order to cleanup the
+# namespace of some subpackages.
+plugin_dir = pkg_resources.resource_filename('jetstream', 'plugins/')
+plugin_id_pattern = r'(?P<plugin>[^\/]*)\/(?P<path>[^:]*):?(?P<revision>(?<=:)[0-9a-f]{5,40})?$'
 
 from jetstream.workflow import Workflow
 from jetstream import formats, plugins
