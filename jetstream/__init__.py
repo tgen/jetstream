1165ece650afbff9deb282cec63f2a7bc2a54402
Ryan Richholt <rrichholt@tgen.org>
2018-02-05 15:10:52 -0700

    updated package-wide var names, added utils to imports

--- jetstream/__init__.py
+++ jetstream/__init__.py
@@ -4,8 +4,8 @@ import pkg_resources
 
 # I'm adding some package wide variables here in order to cleanup the
 # namespace of some subpackages.
-plugin_dir = pkg_resources.resource_filename('jetstream', 'plugins/')
-plugin_id_pattern = r'(?P<plugin>[^\/]*)\/(?P<path>[^:]*):?(?P<revision>(?<=:)[0-9a-f]{5,40})?$'
+PLUGIN_DIR = pkg_resources.resource_filename('jetstream', 'plugins/')
+PLUGIN_ID_PATTERN = r'(?P<plugin>[^\/]*)\/(?P<path>[^:]*):?(?P<revision>(?<=:)[0-9a-f]{5,40})?$'
 
-from . import formats, plugins, config
+from . import formats, plugins, config, utils
 from .workflow import Workflow
