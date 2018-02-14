ca07465441b45b22e9248b04e3162131b130577d
Ryan Richholt <rrichholt@tgen.org>
2018-02-14 12:17:58 -0700

    Removed default imports in order to try and make the cli more snappy But, refer to notes in cli modules for more thoughts on this problem

--- jetstream/__init__.py
+++ jetstream/__init__.py
@@ -7,11 +7,7 @@ PLUGIN_DIR = pkg_resources.resource_filename('jetstream', 'plugins/')
 PLUGIN_ID_PATTERN = r'(?P<plugin>[^\/]*)\/(?P<path>[^:]*):?(?P<revision>(?<=:)[0-9a-f]{5,40})?$'
 
 
-from . import formats, plugins, config, project, launch, utils
-from .workflow import Workflow
-
 # TODO: Move reports.legacy.Project into jetstream.project
 
 # TODO: should project functions walk up the directory tree like git?
 # see this https://gist.github.com/zdavkeos/1098474
-
