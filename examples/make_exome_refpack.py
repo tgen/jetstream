10a9c18fdfa0351784f9e647b5022a1cf924e168
Ryan Richholt <rrichholt@tgen.org>
2018-03-02 13:23:58 -0700

    Updated make_exome_refpack

--- examples/make_exome_refpack.py
+++ examples/make_exome_refpack.py
@@ -75,12 +75,12 @@ import subprocess
 import tempfile
 
 import pkg_resources
-from jetstream.formats.refdict import refdict_to_bedtools_genome
+from jetstream.script_tools.formats.refdict import refdict_to_bedtools_genome
 from jetstream.script_tools.formats import intervals
 
 log = logging.getLogger(__name__)
 
-PICARD_PATH = pkg_resources.resource_filename('jetstream', 'etc/picard.jar')
+PICARD_PATH = pkg_resources.resource_filename('jetstream', 'script_tools/etc/picard.jar')
 BEDTOOLS = 'bedtools'  # TODO im stumped how to add bedtools source in, maybe it
 # needs to be built first and then distributed with a bdist? In that case we'll
 # need to create setup a build tool/script for the project and it adds some
