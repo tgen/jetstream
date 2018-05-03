cce51304c390c147a29bfc240ed89c9877076a47
Ryan Richholt <rrichholt@tgen.org>
2018-05-03 14:32:15 -0700

    added explode feature to legacy config parsers
    This should create a fast path to testing old projects within the
    new system.

--- jetstream/cli/subcommands/legacy.py
+++ jetstream/cli/subcommands/legacy.py
@@ -1,9 +1,9 @@
+import os
 import sys
+import csv
 import argparse
 import logging
-
-from jetstream import utils
-from jetstream.legacy import config
+import jetstream
 
 log = logging.getLogger(__name__)
 
@@ -16,11 +16,35 @@ def build_parser():
 
     parser.add_argument('path')
 
+    parser.add_argument('--json', dest='format', action='store_const',
+                        const='json')
+
+    parser.add_argument('--yaml', dest='format', action='store_const',
+                        const='yaml')
+
+    parser.add_argument('--explode', dest='format', action='store_const',
+                        const='explode')
+
     parser.add_argument('--format',
                         default='yaml',
-                        choices=['yaml', 'json'])
+                        choices=['yaml', 'json', 'explode'])
 
     return parser
+
+
+def records_to_csv(records, outpath):
+    log.debug('to csv: {}\n{}'.format(outpath, records))
+
+    keys = set()
+    for row in records:
+        keys = keys.union(set(row.keys()))
+
+    log.debug('Found keys: {}'.format(keys))
+
+    with open(outpath, 'w') as fp:
+        dw = csv.DictWriter(fp, keys)
+        dw.writeheader()
+        dw.writerows(records)
 
 
 def main(args):
@@ -28,9 +52,21 @@ def main(args):
     args = parser.parse_args(args)
     log.debug('{}: {}'.format(__name__, args))
 
-    c = config.load(args.path)
+    c = jetstream.legacy.config.load(args.path)
 
     if args.format == 'yaml':
-        utils.yaml.dump(c, stream=sys.stdout)
+        jetstream.utils.yaml.dump(c, stream=sys.stdout)
+
     elif args.format == 'json':
-        print(utils.json.dumps(c, indent=4))
+        jetstream.utils.json.dump(c, fp=sys.stdout)
+
+    elif args.format == 'explode':
+        outdir = os.path.splitext(os.path.basename(args.path))[0]
+        os.mkdir(outdir)
+
+        log.critical('Exploding into: {}'.format(outdir))
+        records_to_csv(c['samples'], os.path.join(outdir, 'samples.csv'))
+        records_to_csv(c['data'], os.path.join(outdir, 'data.csv'))
+
+        with open(os.path.join(outdir, 'run_parameters.yaml'), 'w') as fp:
+            jetstream.utils.yaml.dump(c['run_parameters'], stream=fp)
