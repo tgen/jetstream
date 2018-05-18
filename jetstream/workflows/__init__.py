c502cc3619bf0621d7a290912b3f5c860ec24007
Ryan Richholt <rrichholt@tgen.org>
2018-05-18 14:36:35 -0700

    Moved all workflow code to jetstream.workflows

--- jetstream/workflows/__init__.py
+++
@@ -1,4 +0,0 @@
-from jetstream.workflows.workflow import (Workflow, from_node_link_data,
-    to_node_link_data, load, to_cytoscape_json)
-from jetstream.workflows.builder import build_workflow, validate_nodes
-from jetstream.workflows.runner import run_workflow
