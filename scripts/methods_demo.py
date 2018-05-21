b648038efc9ff834767b8d3a7898a6b252c4f825
Ryan Richholt <rrichholt@tgen.org>
2018-05-21 13:47:42 -0700

    methods section demo

--- scripts/methods_demo.py
+++ scripts/methods_demo.py
@@ -8,8 +8,10 @@ def main(workflow_path):
     tasks = list(wf.tasks(data=True))
     tasks.sort(key=lambda t: t[1]['datetime_start'])
 
-    for task in tasks:
-        print(task.get('methods'))
+    for task_id, task in tasks:
+        method = task.get('methods')
+        if method:
+            print(method)
 
 
 if __name__ == "__main__":
