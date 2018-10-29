e8d8159495dae292aa5cd0e8434583a4bc0fc721
Ryan Richholt <rrichholt@tgen.org>
2018-10-29 12:35:11 -0700

    Removed unused project.extra_dirs, config is now a top level dir

--- jetstream/projects.py
+++ jetstream/projects.py
@@ -4,16 +4,15 @@ from jetstream import log, settings
 
 # Project index dir configuration
 index_dir = settings['project_index_dir']
-config_dir = os.path.join(index_dir, 'config')
 history_dir = os.path.join(index_dir, 'history')
 logs_dir = settings['project_logs_dir']
 results_dir = settings['project_results_dir']
 temp_dir = settings['project_temp_dir']
-extra_dirs = settings['project_extra_dirs']
-pid_file = os.path.join(index_dir, 'pid.yml')
-config_file = os.path.join(index_dir, 'config.yml')
-created_file = os.path.join(index_dir, 'created.yml')
-workflow_file = os.path.join(index_dir, 'workflow.yml')
+config_dir = settings['project_config_dir']
+pid_file = os.path.join(index_dir, 'pid')
+config_file = os.path.join(index_dir, 'config')
+created_file = os.path.join(index_dir, 'created')
+workflow_file = os.path.join(index_dir, 'workflow')
 
 
 class NotAProject(Exception):
@@ -33,8 +32,9 @@ class Project:
     <project>
         .
         ├── jetstream
-        │   ├── config_files
-        │   ├── created.yml
+        │   ├── config
+        │   ├── created
+        │   ├── workflow
         │   └── history
         ├── logs
         ├── results
@@ -60,7 +60,6 @@ class Project:
         self.logs_dir = os.path.join(self.path, logs_dir)
         self.results_dir = os.path.join(self.path, results_dir)
         self.temp_dir = os.path.join(self.path, temp_dir)
-        self.extra_dirs = [os.path.join(self.path, d) for d in extra_dirs]
 
         if new:
             self.init()
@@ -81,16 +80,13 @@ class Project:
         os.makedirs(self.results_dir, exist_ok=True)
         os.makedirs(self.temp_dir, exist_ok=True)
 
-        for d in self.extra_dirs:
-            os.makedirs(d, exist_ok=True)
-
-        if not os.path.exists(self.created_file):
+        if os.path.exists(self.created_file):
+            log.info('Reinitialized project: {}'.format(self.path))
+        else:
             with open(self.created_file, 'w') as fp:
                 info = jetstream.utils.Fingerprint()
                 jetstream.utils.yaml_dump(info.serialize(), stream=fp)
             log.info('Initialized project: {}'.format(self.path))
-        else:
-            log.info('Reinitialized project: {}'.format(self.path))
 
     def validate(self):
         if not os.path.exists(self.path):
@@ -129,23 +125,25 @@ class Project:
         if os.path.exists(self.config_file):
             self.config = jetstream.utils.yaml_load(self.config_file)
 
-        config_files = dict()
+        if os.path.isdir(self.config_dir):
+            config_files = dict()
 
-        for path in jetstream.loadable_files(self.config_dir):
-            name = os.path.splitext(os.path.basename(path))[0]
-            config_files[name] = jetstream.load_data_file(path)
+            for path in jetstream.loadable_files(self.config_dir):
+                name = os.path.splitext(os.path.basename(path))[0]
+                config_files[name] = jetstream.load_data_file(path)
 
-        self.config.update(config_files)
+            self.config.update(config_files)
 
         with open(self.config_file, 'w') as fp:
             jetstream.utils.yaml_dump(self.config, fp)
 
     def workflow(self):
-        """Load the total Workflow for this project. """
+        """Load the existing workflow for this project."""
         if os.path.exists(self.workflow_file):
             return jetstream.workflows.load_workflow(self.workflow_file)
         else:
-            return None
+            log.critical('No existing workflow found in this project!')
+            return jetstream.Workflow()
 
     def history(self, paths=False):
         """Find all run ids in this project"""
