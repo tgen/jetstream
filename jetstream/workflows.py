d3982a21ef78e5639097f53e853a7abad2e2af75
Ryan Richholt <rrichholt@tgen.org>
2019-07-10 17:10:57 -0700

    Tasks command accepts globs and general improvements to options

--- jetstream/workflows.py
+++ jetstream/workflows.py
@@ -19,6 +19,7 @@ arguments to template.
 
 """
 import logging
+import glob
 import pickle
 import random
 import re
@@ -89,13 +90,22 @@ class Workflow:
         else:
             return True
 
-    def find(self, pattern, fallback=utils.sentinel):
+    def find(self, pattern, style='regex', fallback=utils.sentinel):
         """Find tasks by matching the pattern with the task ID. If no matches
         are found, and fallback is not set, a ValueError will be raised. If
-        fallback is set, it will be returned when no matches are found."""
-        log.debug('Find: {}'.format(pattern))
-        regex = compile(pattern)
-        matches = set([t for t in self if regex.match(t.name)])
+        fallback is set, it will be returned when no matches are found.
+        Regex patterns are used by default. Set format to glob for glob
+        patterns. """
+        log.debug(f'Find({style}): {pattern}')
+
+        if style == 'regex':
+            regex = compile(pattern)
+            matches = set([t for t in self if regex.match(t.name)])
+        elif style == 'glob':
+            matches = set([t for t in self if glob.fnmatch.fnmatch(t.name, pattern)])
+        else:
+            msg = f'Unrecognized pattern style: {style}'
+            raise ValueError(msg)
 
         if matches:
             return matches
