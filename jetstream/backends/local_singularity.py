c57006115ea23beb14b7bfdefafe0e0e88be62b6
alarsen <alarsen@coh.org>
2020-06-09 06:05:42 -0700

    updated docker nohttps flags

--- jetstream/backends/local_singularity.py
+++ jetstream/backends/local_singularity.py
@@ -42,7 +42,10 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
         self._singularity_pull_cache = {}
         log.info(f'LocalSingularityBackend initialized with {self.cpus} cpus and {self.memory_gb}G memory')
         
-    async def spawn(self, task, allow_cpus_overbooking = True, allow_memory_overbooking = True):
+    async def spawn( self,
+                     task,
+                     allow_cpus_overbooking = True,
+                     allow_memory_overbooking = True ):
         log.debug('Spawn: {}'.format(task))
 
         if 'cmd' not in task.directives:
@@ -52,6 +55,7 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
         input_filenames = task.directives.get( 'input', [] )
         output_filenames = task.directives.get( 'output', [] )
         cpus = task.directives.get('cpus', 0)
+        docker_nohttps = bool( task.directives.get('docker_nohttps', False) )
         
         memory_gb_required_str = task.directives.get('mem', "0G")
         memory_gb_required_value = int( memory_gb_required_str[:-1] )
@@ -92,7 +96,7 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
             async with self._singularity_pull_lock:
                 for i in range( self.cpus ):
                     await self._singularity_run_sem.acquire()
-                opt_https = "--nohttps " if singularity_image.startswith("docker://localhost") else ""
+                opt_https = "--nohttps " if docker_nohttps else ""
                 pull_command_run_string = f'singularity exec --cleanenv {opt_https}{singularity_image} true'
                 log.debug( f'pulling: {pull_command_run_string}' )
                 _p = await create_subprocess_shell( pull_command_run_string,
@@ -148,7 +152,8 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
                 singularity_image=singularity_image,
                 stdin=stdin_fp,
                 stdout=stdout_fp,
-                stderr=stderr_fp
+                stderr=stderr_fp,
+                docker_nohttps=docker_nohttps
             )
 
             task.state.update(
@@ -184,7 +189,7 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
     async def subprocess_sh( self, args, task_name, *, input_filenames=[], output_filenames=[],
                              singularity_image=None, stdin=None, stdout=None, stderr=None,
                              cwd=None, encoding=None, errors=None, env=None,
-                             loop=None, executable='/bin/bash'): 
+                             loop=None, executable='/bin/bash', docker_nohttps=False): 
         try:
             singularity_mounts = set()
             
@@ -210,7 +215,7 @@ class LocalSingularityBackend(jetstream.backends.BaseBackend):
             run_script_filename = f"jetstream/cmd/{task_name}.bash"
             with open( run_script_filename, "w" ) as run_script:
                 run_script.write( args )
-            opt_https = "--nohttps " if singularity_image.startswith("docker://localhost") else ""
+            opt_https = "--nohttps " if docker_nohttps else ""
             command_run_string = f"""singularity exec --cleanenv --nv {opt_https}{singularity_mounts_string} {self._singularity_pull_cache[ singularity_image ]} bash {run_script_filename}"""
 
             log.debug('command_run_string:\n------BEGIN------\n{}\n------END------'.format(command_run_string))
