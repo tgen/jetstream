147034e9279b05201cfc7ce4284378f57df28c8d
Bryce Turner <40504111+bryce-turner@users.noreply.github.com>
2021-10-25 11:43:09 -0700

    Version bump to 1.6.2

--- jetstream/__init__.py
+++ jetstream/__init__.py
@@ -8,7 +8,7 @@ import yaml
 
 __author__ = 'Ryan Richholt'
 __email__ = 'rrichholt@tgen.org'
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 
 
 # Configure parallel library dependencies (Used by numpy)
