11e186903cfba590021ae911e983d57c42c67b49
Bryce Turner <bturner@tgen.org>
2023-01-11 18:03:45 -0700

    Version bump to v1.7.2

--- jetstream/__init__.py
+++ jetstream/__init__.py
@@ -9,7 +9,7 @@ import yaml
 __author__ = 'Ryan Richholt'
 __maintainer__ = 'Bryce Turner'
 __email__ = 'bturner@tgen.org'
-__version__ = '1.7.1'
+__version__ = '1.7.2'
 
 
 # Configure parallel library dependencies (Used by numpy)
